ccfbfb1afc2fd104f6b8a83ed97ef8fb46b16b42
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-12-30 15:56:41 +0700

    Update uspGetLayThucAn.sql
    - Cập nhật query phần bỏ sót.

--- Procedures/uspGetLayThucAn.sql
+++ Procedures/uspGetLayThucAn.sql
@@ -5,10 +5,9 @@ IF OBJECT_ID('uspGetLayThucAn') IS NOT NULL
 DROP PROC uspGetLayThucAn
 GO
 CREATE PROC uspGetLayThucAn
-	@TenThucAn nvarchar(50)
 AS
 BEGIN
 	select * from ThucAn;
 END
 --GO
-EXEC uspGetLayThucAn 'Trái Cây Tô';
+EXEC uspGetLayThucAn
