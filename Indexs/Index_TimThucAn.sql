bbfc22383e36a3ad0946645909bf48c8041fbeaa
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2020-01-02 16:49:33 +0700

    Update Index_TimThucAn.sql
    -Update Index cho Tìm Kiếm Thức Ăn.

--- Indexs/Index_TimThucAn.sql
+++ Indexs/Index_TimThucAn.sql
@@ -4,6 +4,6 @@ drop index if exists Index_TimThucAn on ThucAn
 
 create unique index Index_TimThucAn
 on ThucAn(TenThucAn)
-include(IDThucAn, Gia)
+include(IDThucAn, IDLoaiThucAn, Gia)
 
-select * from ThucAn where TenThucAn like 'x%'
+select * from ThucAn where TenThucAn like 'Xoài'
